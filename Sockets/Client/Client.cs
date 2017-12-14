6c10258c817f7e9be13a4eca1fe965c6300d58b9
Nematin <34038234+Nematin@users.noreply.github.com>
2017-12-14 23:50:38 +0500

    Лабораторная №1

---
+++ Sockets/Client/Client.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        static Socket sendingSocket;
+        static Socket listeningSocket;
+        
+
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Client: " + Dns.GetHostName());
+
+                sendingSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                Task sendTask = new Task(Send);
+                sendTask.Start();
+
+                Task recieveTask = new Task(Recieve);
+                recieveTask.Start();
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static void Send()
+        {
+            try
+            {
+                while (true)
+                {
+
+                    string hostname = Dns.GetHostName();
+                    string message = "OK: " + hostname + "\n";
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+
+                    EndPoint remotePoint = new IPEndPoint(IPAddress.Broadcast, 5000);
+
+                    sendingSocket.EnableBroadcast = true;
+                    sendingSocket.SendTo(data, remotePoint);
+                    Console.WriteLine("Отправил!");
+                    Thread.Sleep(4000);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                CloseSend();
+            }
+
+        }
+
+        private static void Recieve() //Вся запара тут
+        {
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 6000);
+
+            try
+            {
+                //Прослушиваем по адресу
+                listeningSocket.Bind(localEndPoint);
+
+                while (true)
+                {
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+
+                    //адрес, с которого пришли данные
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+                    //EndPoint remoteIp = new IPEndPoint(IPAddress.Loopback, 0);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+                    // получаем данные о подключении
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    // выводим сообщение
+                    Console.WriteLine("В сети {0}:{1} - {2}", remoteFullIp.Address.ToString(), remoteFullIp.Port, builder.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                CloseRecieve();
+            }
+        }
+
+        // закрытие сокета
+        private static void CloseSend()
+        {
+            if (sendingSocket != null)
+            {
+                sendingSocket.Shutdown(SocketShutdown.Both);
+                sendingSocket.Close();
+                sendingSocket = null;
+            }
+        }
+
+        private static void CloseRecieve()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+        }
+    }
+}
+
---
+++ Sockets/Server_v1/Server.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Server_v1
+{
+    class Server
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите IP: ");
+            var localIP = IPAddress.Parse(Console.ReadLine());
+            
+            int serverPort = 5000;
+
+            IPEndPoint localEndPoint = new IPEndPoint(localIP, serverPort);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.EnableBroadcast = true;
+
+            try
+            {
+                StringBuilder builder = new StringBuilder();
+
+                socket.Bind(localEndPoint);
+
+                EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint remoteEndPointClient = new IPEndPoint(IPAddress.Broadcast, 6000);
+
+                string hostname = "Server: " + Dns.GetHostName();
+                byte[] _data = Encoding.Unicode.GetBytes(hostname);
+
+                Console.WriteLine("Ready to receive!");
+                Console.WriteLine("IP: " + localEndPoint);
+                byte[] data = new byte[1024];
+                int i = 0;
+
+                while (true)
+                {
+
+                    do
+                    {
+                        int recv = socket.ReceiveFrom(data, ref remoteEndPoint);
+                        string stringData = Encoding.Unicode.GetString(data, 0, recv);
+                        Console.WriteLine("[" + i + "]" + "Received: {0} from: {1}", stringData, remoteEndPoint.ToString());
+                        i++;
+                    } while (socket.Available > 0);
+
+                    socket.SendTo(_data, remoteEndPointClient);
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadKey();
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            
+
+           
+
+        }
+    }
+}
